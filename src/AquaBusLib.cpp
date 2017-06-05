a65d58507e16667807e56bdfcb3a5bb2838c76f1
Stonyx <info@stonyx.com>
2017-06-05 12:45:32 -0400

    Added FreeModLib library initialization and polling function calls and callback functions.

--- src/AquaBusLib.cpp
+++ src/AquaBusLib.cpp
@@ -1,6 +1,44 @@
+// AquaBus Library
+
+// Include header files
+#include "modbus/include/mb.h"
 #include "AquaBusLib.h"
 
 // Constructor
 AquaBusLib::AquaBusLib()
 {
+}
+
+// Function that needs be called from the sketch setup function
+AquaBusLib::setup()
+{
+  // Initialize and enable the FreeModBus library
+  eMBInit(MB_RTU, 0x00, 0, 19200, MB_PAR_EVEN);
+  eMBEnable();
+}
+
+// Function that needs to be called fro the sketch loop function
+AquaBusLib::loop()
+{
+  eMBPoll();
+}
+
+// Callback function called to get or set the value of a Coil Register
+eMBErrorCode eMBRegCoilsCB (UCHAR *pucRegBuffer, USHORT usAddress, USHORT usNCoils, eMBRegisterMode eMode)
+{  
+}
+
+// Callback function called to get the value of an Input Discrete Register
+eMBErrorCode eMBRegDiscreteCB (UCHAR *pucRegBuffer, USHORT usAddress, USHORT usNDiscrete)
+{  
+}
+
+// Callback function called to get or set the value of a Holding Register
+eMBErrorCode eMBRegHoldingCB (UCHAR *pucRegBuffer, USHORT usAddress, USHORT usNRegs, eMBRegisterMode eMode)
+{  
+}
+
+// Callback function called to get the value of an Input Register
+eMBErrorCode eMBRegInputCB (UCHAR *pucRegBuffer, USHORT usAddress, USHORT usNRegs)
+{  
 }
--- src/AquaBusLib.h
+++ src/AquaBusLib.h
@@ -1,9 +1,8 @@
-// Ensure this header file is only included once
+// AquaBus Library
+
+// Ensure this header file is included only once
 #ifndef AquaBusLib_h
 #define AquaBusLib_h
-
-// Included needed header files
-#include "modbus/include/mb.h"
 
 // The AquaBusLib class
 class AquaBusLib
@@ -11,6 +10,8 @@ class AquaBusLib
   // User-accessible "public" interface
   public:
     AquaBusLib();
+    setup();
+    loop();
 
   // Library-accessible "private" interface
   private:
